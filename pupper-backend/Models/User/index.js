5635692fa9a374add02483ffe1dea252ae4c8b2d
Tristan Elliott <tristanelliott7@gmail.com>
2020-08-18 19:34:41 -0300

    adding the userSchema and login ability to our app

---
+++ pupper-backend/Models/User/index.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const Schema = mongoose.Schema;
+const salt = bcrypt.genSaltSync(10)
+
+
+const userSchema = new Schema({
+  username:{type:String,required:true,unique:true},
+  email:String,
+  bio:String,
+  password:{type:String,required:true}
+
+})
+
+userSchema.pre('save',function(done){
+  //this should run before every save call to the user model
+  let user = this;
+  if(!user.isModified("password")){
+    return done();
+  }
+  bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash(user.password, salt, function(err, hashedPassword) {
+      if(err){return done(err);}
+      user.password = hashedPassword;
+      done();
+    });
+});
+});
+
+userSchema.methods.checkPassword = function(guess,done){
+  bcrypt.compare(guess,this.password, function(err,isMatch){
+    done(err,isMatch)
+  })
+}
+
+
+
+
+const User = mongoose.model('User',userSchema);
+
+module.exports = User
--- pupper-backend/index.js
+++ pupper-backend/index.js
@@ -1,25 +1,47 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const User = require('./Models/User')
 
 const app = express()
-const port = 3000
+const port = 3001
 const url ='';
 const db = mongoose.connection
 
-mongoose.connect(url,{useNewUrlParser:true});
+mongoose.connect(url,{useUnifiedTopology: true,
+useNewUrlParser: true,});
 db.once('open',()=>{
   console.log('Database connected',url)
 })
 db.once('error',err=>{
   console.error('connection error',err)
 })
+app.use(bodyParser.json())
 
 app.get('/',(req,res)=>{
   res.send('THis should be all the pups spotted')
 })
 
-app.post('/spotting',()=>{
-  res.send('created new pup spotting')
+app.post('/register',(req,res,next)=>{
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ username:username},function(err,user){
+    if(err){ return next(err)}
+    if(user){
+        return res.send('user already here m8')
+    }
+    const newUser = new User({
+      username,
+      password
+    });
+    newUser.save(next)
+    console.log()
+    res.send('created new user')
+
+  })
+
 })
 
 app.listen(port,()=>{

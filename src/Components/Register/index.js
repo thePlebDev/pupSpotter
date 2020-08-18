ae44ed5f794806545be1e93512cb8bef06b36fc2
Tristan Elliott <tristanelliott7@gmail.com>
2020-08-18 19:55:26 -0300

    adding the styles to the register form

--- src/Components/Register/index.js
+++ src/Components/Register/index.js
@@ -1,40 +1,79 @@
 import React,{useState} from 'react';
+import styled from 'styled-components';
+
 
 import useRegister from '../../Hooks/UseRegister'
 import {registerValidation} from '../../utils/Validation';
+
+
+const Form = styled.form`
+  width:80%;
+  margin: 0 auto;
+`
+const Input = styled.input`
+width:100%;
+font-size:18px;
+padding:10px;
+border-radius:4px;
+outline:none;
+
+margin-top:5px;
+border:1 px solid #05386B;
+margin-bottom:10px;
+`
+
+const Text = styled.div`
+font-size:1.2em;
+font-weight:600;
+letter-spacing:1px;
+`
+const Button = styled.div`
+background-color:#05386B;
+padding:10px 36px;
+color:#5CDB95;
+border-radius:4px;
+margin:10px auto;
+font-size:1.2em;
+letter-spacing:1px;
+outline:none;
+text-align:center;
+`
+
+
+
 const Register = () =>{
   const {name,bio,email,password,handleChange,handleSubmit} = useRegister(registerValidation)
 
   return(
     <div>
-      <form onSubmit={(e)=>handleSubmit(e)}>
+      <Form onSubmit={(e)=>handleSubmit(e)}>
         <div>
           <label>
-            Name
-            <input type='text' value={name} onChange={(e)=>handleChange(e)} name='name' />
+            <Text>Name</Text>
+            <Input type='text' value={name} onChange={(e)=>handleChange(e)} name='name' />
           </label>
         </div>
         <div>
           <label>
-            Email
-            <input type='email' value={email} onChange={(e)=>handleChange(e)} name='email' />
+          <Text>  Email </Text>
+            <Input type='email' value={email} onChange={(e)=>handleChange(e)} name='email' />
           </label>
         </div>
         <div>
           <label>
-            Bio
-            <input type='text' value={bio} onChange={(e)=>handleChange(e)} name='bio' />
+            <Text> Bio </Text>
+            <Input type='text' value={bio} onChange={(e)=>handleChange(e)} name='bio' />
           </label>
         </div>
         <div>
           <label>
-          Password
-            <input type='password' value={password} onChange={(e)=>handleChange(e)} name='password' />
+          <Text> Password </Text>
+            <Input type='password' value={password} onChange={(e)=>handleChange(e)} name='password' />
           </label>
         </div>
-        <button type="Submit">Register</button>
+        <Button type="Submit">Register</Button>
 
-      </form>
+      </Form>
     </div>
   )
 }

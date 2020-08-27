69060367db185a7168ff7c8e65b7ac6101ddeffe
Tristan Elliott <tristanelliott7@gmail.com>
2020-08-26 21:30:56 -0300

    adding some tests

---
+++ pupper-backend/test/test1.js
@@ -0,0 +1,10 @@
+const chai = require('chai')
+const expect = require('chai').expect;
+
+const numbers = [1,2,3,4,5];
+
+describe('sum',()=>{
+  it('shoule add all the numbers',()=>{
+    expect(15).to.equal(15)
+  })
+})

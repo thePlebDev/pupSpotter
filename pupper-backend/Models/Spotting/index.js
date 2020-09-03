f20d67265298084103accb608f5f289765092d9f
Tristan Elliott <tristanelliott7@gmail.com>
2020-09-03 20:24:40 -0300

    adding description to the spotting schema

--- pupper-backend/Models/Spotting/index.js
+++ pupper-backend/Models/Spotting/index.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema
 
 const spottingSchema = new Schema({
   name:{type:String,required:true},
+  description:{type:String},
   date:{type:Date,default:Date.now},
   image:{type:String},
   location:{
@@ -12,7 +13,7 @@ const spottingSchema = new Schema({
     lon:Number
   },
   user:{type:Schema.Types.ObjectId,ref:"User"} // the type will be a an ObjectId and during population the model to be used will be the User
-  
+
 
 })
 

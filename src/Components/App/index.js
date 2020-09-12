21b19c5a99ce8c427d8d9b4dd23347dbc48fe88d
Tristan Elliott <tristanelliott7@gmail.com>
2020-09-11 22:35:20 -0300

    creating the basic outline for popup

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -10,15 +10,15 @@ import Profile from '../Profile';
 
 
 const App =()=>{
-  const [navStatus, setNavStatus] = useState(false) // BUG: resets to false on page reload
+  const [loggedIn,setLoggedIn] = useState(false)
   return(
     <div>
-      <NavBar status={navStatus} setNavStatus={setNavStatus}/>
+      <NavBar loggedIn={loggedIn} />
       <Switch>
-        <Route exact path ='/' component={Map}/>
+        <Route exact path ='/' component={Map} render={(props)=><Map setLoggedIn={setLoggedIn} {...props} /> }/>
         <Route exact path ='/pupSpotting' component={PupForm} />
         <Route exact path ='/register' component={Register} />
-        <Route exact path ='/login' render={(props)=><Login setNavStatus={setNavStatus} {...props} /> } />
+        <Route exact path ='/login' component={<Login />} />
         <Route exact path ='/profile' component={<Profile/>}/>
       </Switch>
     </div>
--- src/Components/Leaflet/index.js
+++ src/Components/Leaflet/index.js
@@ -1,13 +1,23 @@
 import React,{useEffect,useState} from 'react';
 import {Map,TileLayer,Marker,Popup} from 'react-leaflet';
 import axios from 'axios'
+import {backendUrl} from '../../utils/Constants'
 
-
-const MapContainer= ()=>{
+const MapContainer= ({setLoggedIn})=>{
 const [state] = useState({lat:45.897820,lng:-64.368279,zoom:13})
+
 const [mapData,setMapData] = useState('');
 console.log(mapData)
 
+
+useEffect(()=>{
+  axios.get(`${backendUrl}isAuthenticated`,{withCredentials:true})
+  .then(data=>{
+    if(data.data.status === 200){
+      setLoggedIn(true)
+    }})
+  .catch(e=>console.error('error------> ' + e))
+},[])
 
   useEffect(()=>{
     // make a get request to get all of the spotting
@@ -47,8 +57,16 @@ console.log(mapData)
         }
           <Marker position={[state.lat,state.lng]} >
             <Popup>
-              <div>Dog Here</div>
+                <div style={{display:'flex',justifyContent:'center',alignItems:'center',flexDirection:'column'}}>
+                  <img  src="https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80" style={{width:'90px',height:'90px',borderRadius:'30%'}} alt='puppy'></img>
+                  <div style={{marginTop:'15px',alignSelf:'flex-end'}}>
+                    <button>Edit</button>
+                    <button>Delete</button>
+                  </div>
+                  <button>Give treat</button>
+                </div>
             </Popup>
+
           </Marker>
 
         </Map>

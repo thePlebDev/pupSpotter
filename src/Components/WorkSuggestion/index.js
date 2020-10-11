e1bd91282ed3ae6737a995b3893cfb10fd85292e
Tristan Elliott <tristanelliott7@gmail.com>
2020-10-11 17:13:36 -0300

    adding the big accordian effect

--- src/Components/WorkSuggestion/index.js
+++ src/Components/WorkSuggestion/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React,{ useState,useRef } from 'react';
 import styled from 'styled-components';
-import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
+import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
 
-import WorkSuggestionForm from '../WorkSuggestionForm'
-import WorkListItem from '../WorkListItem'
+import WorkSuggestionForm from '../WorkSuggestionForm';
+import WorkListItem from '../WorkListItem';
 
 const ContainerList = styled.div`
   display:flex;
@@ -34,22 +34,51 @@ const Title = styled.div`
   letter-spacing:1px;
   padding-left:5px;
 `
+const Display = styled.div`
+  transition: max-height 0.2s ease-out;
+  max-height:${props=>props.clicked ? props.height + "px": '0'};
+  overflow:hidden
+`
+const Arrow = styled.div`
+color:white;
+padding-right:5px;
+padding-top:5px;
+transform-origin:43% 50%;
+transition: all 0.3s;
+transform:${props=>props.show ?'rotate(180deg)':''};
+
+`
+
 
 
 const WorkSuggestion =()=>{
+  const [show,setShow] = useState(false)
+  const [scrollHeight,setScrollHeight] = useState('')
+  const node = useRef(null)
+
+  const handleClick =()=>{
+    setShow(!show)
+    setScrollHeight(node.current.scrollHeight)
+    console.log(scrollHeight)
+  }
 
   return(
     <ContainerList data-testid="container">
-      <div style={{display:'flex',justifyContent:'space-between',width:'100%'}}>
+      <div style={{display:'flex',justifyContent:'space-between',width:'100%',alignItems:'center'}} onClick={()=>{handleClick() }}>
         <Title>ADD</Title>
-        <PlayCircleFilledIcon style={{color:'white','padding-right':'5px','padding-top':'5px',transformOrigin:'center',transform:'rotate(90deg)'}}/>
+        <Arrow show={show}>
+          <ArrowUpwardIcon />
+        </Arrow>
       </div>
-        {
-          data.map((item,index)=>{
-            return<WorkListItem key={index} data-testid="items" items={item.words}/>
-          })
-        }
-        <WorkSuggestionForm/>
+        <Display clicked={show} ref={node} height={scrollHeight} >
+            {
+              data.map((item,index)=>{
+                return<WorkListItem key={index} data-testid="items" items={item.words}/>
+              })
+            }
+
+            <WorkSuggestionForm/>
+        </Display>
     </ContainerList>
   )
 }
